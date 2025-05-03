2efc38517a4d4fbe13657868a4b3dad06dd7b819
Avneet Singh <singhab2024@gmail.com>
2025-05-03 02:10:20 -0400

    Readme updates + conc

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 This repository was designed to test and validate beliefs about operating systems and computer architecture so that I use it for my own reference later down the line.
 
-So far I have tested:
-* Ram vs Cache latency using C++: using my experiment, I found that cache access was about 5x faster
-* Single vs Multithreaded using C++: 
+### Ram vs Cache latency using C++ | raw-vs-l1-latency.cpp
+
+Using my experiment, I found that cache access was about 5x faster - I tested this by using an inorder traversal vs random traversal of a very large array, causing the random approach to have to run to RAM to fetch the values.
+
+### Single vs Multithreaded using C++ | concurrency.cpp
+
+Want to see after how many operations is it actually beneficial to use multithreading over single threaded. I wanted to explore to see if there's a trade off from overhead of locking (through a mutex) the shared memory buffer (counter) that is being updated or not when theres a lot of processes. I want to understand the tradeoffs of multithreading and see when it can truly be useful.
--- concurrency.cpp
+++ concurrency.cpp
@@ -3,7 +3,9 @@
 #include <mutex>
 #include <thread>
 
-// 1) A small helper function that runs any callable and returns elapsed µs
+int counter1{0};
+int counter2{0};
+
 template <typename F>
 long long measure_us(F&& fn) {
   using Clock = std::chrono::steady_clock;
@@ -16,29 +18,36 @@ long long measure_us(F&& fn) {
   return std::chrono::duration_cast<us>(end - start).count();
 }
 
-int counter1{0};
-int counter2{0};
-
 void increment_counter(int number_increments) {
   for (int i = 0; i < number_increments; ++i) {
     ++counter1;
   }
 }
 
+void threaded_counter(int cycles) {}
+
 long long p1(int cycles) {
   return measure_us([cycles] { increment_counter(cycles); });
 }
 
-int p2(int cycles) { return 0; }
+long long p2(int cycles) {
+  return measure_us([cycles] { threaded_counter(cycles); });
+}
 
 int main(int argc, char const* argv[]) {
   if (argc != 2) {
+    std::cout << "Hey, you forgot to tell me how many cycles to run!";
     return 1;
   }
 
   int cycles = std::stoi(argv[1]);
+
   long long time1 = p1(cycles);
-  std::cout << "Time elapsed with no concurrency " << time1 << " microseconds! "
+  std::cout << "Time elapsed with no concurrency " << time1 << " microseconds!"
+            << '\n';
+
+  long long time2 = p2(cycles);
+  std::cout << "Time elapsed with concurrency " << time2 << " microseconds!"
             << '\n';
 
   return 0;
