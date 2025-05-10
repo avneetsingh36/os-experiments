da7143884ad1af0772a96c0698be4a52d2486486
Avneet Singh <singhab2024@gmail.com>
2025-05-09 22:26:42 -0700

    adding an experiment to test atomic memory access

---
+++ atomic_mem_access.cpp
@@ -0,0 +1,29 @@
+#include <atomic>
+#include <iostream>
+#include <thread>
+
+std::atomic<int> counter{0};
+
+void increment(int cycles) {
+  for (int i = 0; i < cycles; ++i) {
+    ++counter;
+  }
+}
+
+int main() {
+  std::cout << "Counter: " << counter << '\n';
+
+  int cycles;
+  std::cout << "How many cycles do you want to run: ";
+  std::cin >> cycles;
+
+  std::thread t1(increment, cycles);
+  std::thread t2(increment, cycles);
+
+  t1.join();
+  t2.join();
+
+  std::cout << "Counter: " << counter << '\n';
+
+  return 0;
+}
