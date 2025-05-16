19de73dbb7dda86ec8624beebe99249573f3fdbe
Avneet Singh <singhab2024@gmail.com>
2025-05-15 17:43:53 -0700

    more semantic c++

--- process_sim.cpp
+++ process_sim.cpp
@@ -1,3 +1,5 @@
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
 #include <iostream>
@@ -5,22 +7,29 @@
 int myNumber = 100;
 
 int main() {
-  // process ID will be 0 for a successful child process
-
   pid_t pid = fork();
 
   if (pid < 0) {
-    std::cout << "Fork failed";
-  } else if (pid == 0) {
+    std::perror("fork");
+    return 1;
+  }
+
+  if (pid == 0) {
+    // Child
     myNumber = 200;
-    std::cout << "I am the child process with PID: " << pid << '\n';
+    std::cout << "I am the child process with PID: " << getpid() << '\n';
     std::cout << "My number from the child process is: " << myNumber << '\n'
               << '\n';
+    return 0;
+
   } else {
     // will ensure that we wait for the child to go first
     // notice how the VAS is diffferent the second the fork() happens
     int status;
-    waitpid(pid, &status, 0);
+    if (waitpid(pid, &status, 0) < 0) {
+      std::perror("waitpid");
+    }
+
     std::cout << "Hello from the parent process with PID: " << pid << '\n';
     std::cout << "My number is from the parent process is: " << myNumber
               << '\n';
